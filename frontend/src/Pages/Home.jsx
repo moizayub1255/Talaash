5456f4d52cd59e8155b008160e907404ad165ff9
Moiz <moizayub401@gmail.com>
2025-05-28 21:54:35 +0500

    changes

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,8 +5,7 @@ const Home = () => {
   return (
     <Headandfoot>
       <div className="container">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the main content of the home page.</p>
+        <h1>Home</h1>
       </div>
     </Headandfoot>
   );
